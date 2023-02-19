9419a13fec6d3857dffb1b3e5f804db91588a621
Beccio00 <riccardo.becciolini@stud.unifi.it>
2023-02-19 12:58:33 +0100

    Iniziato lista spesa

--- ListaSpesa.h
+++ ListaSpesa.h
@@ -5,12 +5,22 @@
 #ifndef PROJECTLAB_LISTASPESA_H
 #define PROJECTLAB_LISTASPESA_H
 
+#include <iostream>
+#include <list>
 #include "Subject.h"
+#include "Prodotto.h"
+#include "Observer.h"
 
 class ListaSpesa : public Subject{
 public:
+    ListaSpesa(std::string n): name(n){};
+
+
 
 private:
+    std::string name;
+    std::list<Prodotto> prodotti;
+    std::list<Observer*> observers;
 
 };
 
